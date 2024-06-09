51eabfb4305165f017fbf4d33863b80edbc32f4f
EatablePaimon <3378057121@qq.com>
2024-06-09 21:52:52 +0800

    :sparkles:add a login ui

---
+++ star_query_rail_client/__main__.py
@@ -0,0 +1,24 @@
+import sys
+
+from PyQt5.QtCore import Qt, QTranslator, QLocale
+from PyQt5.QtWidgets import QApplication
+from qfluentwidgets import setThemeColor, FluentTranslator, setTheme, Theme, SplitTitleBar, isDarkTheme
+
+from star_query_rail_client.view.login_window import LoginWindow
+
+if __name__ == '__main__':
+    # enable dpi scale
+    QApplication.setHighDpiScaleFactorRoundingPolicy(
+        Qt.HighDpiScaleFactorRoundingPolicy.PassThrough)
+    QApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
+    QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps)
+
+    app = QApplication(sys.argv)
+
+    # Internationalization
+    translator = FluentTranslator(QLocale())
+    app.installTranslator(translator)
+
+    w = LoginWindow()
+    w.show()
+    app.exec_()
